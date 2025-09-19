25558a6f042327421a30dcb898b8a8b9732cb1e8
igor <myzzon84@gmail.com>
2025-09-19 21:29:24 +0300

    commit

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -4,6 +4,7 @@ import { homePageStore } from "../store/homePageStore"
 const CategoryPage = () => {
   const productsByCategory = homePageStore(state => state.productsByCategory);
   const loadingCategoryProduct = homePageStore(state => state.loadingCategoryProduct);
+  console.log(productsByCategory);
   return (
     <ProductsPage products={productsByCategory} loading={loadingCategoryProduct}>
         
